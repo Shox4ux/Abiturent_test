9f8793b7e631a636304255083d25bd8e85ff2988
JonToshpulatov <yusuftoshpulatov980@gmail.com>
2022-11-09 20:39:48 +0500

    new

---
+++ lib/ui/bottom_navigation/profile/my_subscribe/my_subscribe.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MySubscribe extends StatefulWidget {
+  const MySubscribe({super.key});
+
+  @override
+  State<MySubscribe> createState() => _MySubscribeState();
+}
+
+class _MySubscribeState extends State<MySubscribe> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
